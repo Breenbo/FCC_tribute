b64ac7bb42ed0862dad61748db402fb349f49011
breenbo <berrehuelbruno@gmail.com>
2018-07-22 02:08:55 -0400

    text and custom font

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var cards = document.getElementsByClassName('card')
 
 // create function to move screen on card hover.
-function rideau (name, sens = 'down') {
-  var screenName = name + '_screen'
-  var element = document.getElementById(screenName)
-  var rideauHeight, opacity
-  sens === 'down' ? rideauHeight = '80%' : rideauHeight = '-5%'
-  sens === 'down' ? opacity = 0 : opacity = 1
-  element.style.transitionDuration = '1.5s'
-  element.style.top = rideauHeight
-  element.style.opacity = opacity
-};
+// function rideau (name, sens = 'down') {
+//   var screenName = name + '_screen'
+//   var element = document.getElementById(screenName)
+//   var rideauHeight, opacity
+//   sens === 'down' ? rideauHeight = '80%' : rideauHeight = '-5%'
+//   sens === 'down' ? opacity = 0 : opacity = 1
+//   element.style.transitionDuration = '1.5s'
+//   element.style.top = rideauHeight
+//   element.style.opacity = opacity
+// };
 
 // apply rideau to all cards
 // for (const card of cards) {
